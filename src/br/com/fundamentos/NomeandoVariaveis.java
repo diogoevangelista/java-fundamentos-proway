9f6b624beb3bc215b60df37ff6b41e6efbb286c6
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 17:45:18 -0300

    Nomeando variaveis

---
+++ src/br/com/fundamentos/NomeandoVariaveis.java
@@ -0,0 +1,28 @@
+package src.br.com.fundamentos;
+
+public class NomeandoVariaveis {
+	
+	public static void main(String[] args) {
+		//valido separado por underline _
+		String variavel_separada_por_underline;
+		
+		//padrão camelcase
+		String variavelSeparadaPorUnderline;
+		
+		//valido porem estranho
+		int _numero;
+		
+		System.out.println(_numero);
+		
+		//válido porem mais estranho ainda! parece PHP
+		int $outroNumero;
+		
+		System.out.println($outroNumero);
+		
+		//não pode iniciar com caracteres especiais
+		String 1um;
+		
+		int @arroba;
+	}
+
+}
