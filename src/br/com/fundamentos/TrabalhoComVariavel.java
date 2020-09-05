03b36fae040d789e36cffdea293f75f3f14a5b51
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 17:57:24 -0300

    Trabalhando com Variavel

--- src/br/com/fundamentos/TrabalhoComVariavel.java
+++ src/br/com/fundamentos/TrabalhoComVariavel.java
@@ -27,6 +27,27 @@ public class TrabalhoComVariavel {
 		long numeroInteiroGrande = 25l;
 		
 		System.out.println("numero inteiro com long -> " + numeroInteiroGrande);
+		
+		double precoProduto = 9.43;
+		
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+		
+		char turmaAluna1 = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		String nomePessoa = "Diogo";
+		
+		long populacaoUberlandia = 650000;
+		System.out.println(populacaoUberlandia);
+		
+		long populacaoMundial = 7000000000L;
+		System.out.println(populacaoMundial);
+		
+		float saldoConta = 1030.43F;
+		System.out.println(saldoConta);
+		
 	}
 	
 	
