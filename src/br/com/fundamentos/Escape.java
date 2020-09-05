6258540890a7a0def64db854732bd2c6981fd619
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 16:06:16 -0300

    1 - aula caracter Escape

---
+++ src/br/com/fundamentos/Escape.java
@@ -0,0 +1,16 @@
+package src.br.com.fundamentos;
+
+public class Escape {
+
+	public static void main(String[] args) {
+		System.out.println("Usando o \"eclipse\"");
+		System.out.println("Usando o \neclipse");
+		System.out.println("Usando o \beclipse");
+		System.out.println("Usando o \teclipse");
+		System.out.println("Usando o \feclpse");
+		System.out.println("Usando o \reclipse");
+		System.out.println("Usando o \'eclipse");
+		System.out.println("Usando o \\eclipse");
+		
+	}
+}
