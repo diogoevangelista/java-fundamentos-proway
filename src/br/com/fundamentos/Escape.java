0ad62652a1fe26ffa103032cd99c02fc05f71764
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 16:43:11 -0300

    1 - aula fundamentos

--- src/br/com/fundamentos/Escape.java
+++ src/br/com/fundamentos/Escape.java
@@ -4,13 +4,21 @@ public class Escape {
 
 	public static void main(String[] args) {
 		System.out.println("Usando o \"eclipse\"");
+		
 		System.out.println("Usando o \neclipse");
+		
 		System.out.println("Usando o \beclipse");
+		
 		System.out.println("Usando o teclipse");
+		
 		System.out.println("Usando o \feclpse");
+		
 		System.out.println("Usando o \reclipse");
+		
 		System.out.println("Usando o \'eclipse");
+		
 		System.out.println("Usando o \\eclipse");
+		
 		
 	}
 }
