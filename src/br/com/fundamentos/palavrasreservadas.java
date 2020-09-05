43f3459144deb95d568a1a82cc4374a1e6dbd358
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 16:25:48 -0300

    palavras reservadas

---
+++ src/br/com/fundamentos/palavrasreservadas.java
@@ -0,0 +1,13 @@
+package src.br.com.fundamentos;
+
+public class palavrasreservadas {
+	/*
+	 * Palavras reservadas são palavras que a linguagem ultiliza
+	 * como exemplo: int, string, double
+	 * http://www.linhadecodigo.com.br/artigo/83/as-52-palavras-reservadas-do-java.aspx
+	 */
+
+	int numero;
+	
+}
+
