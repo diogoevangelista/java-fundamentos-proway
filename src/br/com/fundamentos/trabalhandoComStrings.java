276e566b42034766e25b91b633868c08d9f00fcc
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-05 18:21:39 -0300

    Trabalhando com String

---
+++ src/br/com/fundamentos/trabalhandoComStrings.java
@@ -0,0 +1,12 @@
+package src.br.com.fundamentos;
+
+	public class trabalhandoComStrings {
+		public static void main (String[] args) {
+			
+		String nome = "Maria";
+		int idade = 30;
+		
+		System.out.println(nome + " tem " + idade + " anos ");
+		
+		}
+}
