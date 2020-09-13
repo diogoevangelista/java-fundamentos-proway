fbac786ad92a5fef1d4004feb246f5f7847a81a5
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-13 18:54:10 -0300

    entrada de dados

---
+++ src/br/com/fundamentos/EntradaDados.java
@@ -0,0 +1,40 @@
+package src.br.com.fundamentos;
+
+import java.util.Scanner;
+
+public class EntradaDados {
+	
+		public static void main(String[] args) {
+			Scanner entrada = new Scanner (System.in);
+			
+			System.out.println("Nome: ");
+			String nome = entrada.nextLine();
+			
+			System.out.println("Peso: ");
+			int peso = entrada.nextInt();
+			
+			System.out.println("Altura: ");
+			double altura = entrada.nextDouble();
+			
+			System.out.println("Sexo (1 para 'M' ou outro numero para'F'): ");
+			char sexo = entrada.nextShort() == 1 ? 'M' : 'F';
+			
+			double imc = peso / (altura * altura);
+			
+			//________________A___________________|| __________________B_______________
+			if ((sexo == 'F' && imc < 19.1)       || (sexo == 'M' && imc < 20.7)) {
+				System.out.println("Abaixo do peso.");
+			}
+			else if ((sexo == 'F' && imc <= 25.8) || (sexo == 'M' && imc < 26.4)) {
+				System.out.println("Peso ideal.");
+			}
+			else if ((sexo == 'F' && imc <= 27.3) || (sexo == 'M' && imc < 27.8)) {
+				System.out.println("Acima do peso.");
+			}
+			
+			
+			
+		}
+		
+
+}
