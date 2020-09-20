34aa1d84cdeb775d802a005f8c6ea4ffe0e1de54
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-20 16:24:52 -0300

    tentawhile2

---
+++ src/br/com/fundamentos/TestaWhile2.java
@@ -0,0 +1,24 @@
+package src.br.com.fundamentos;
+
+public class TestaWhile2 {
+	
+		public static void main(String[] args) {
+			
+			int contador = 0;
+			
+			while (contador <= 10) {
+				
+				int total = 0;
+				total = total + contador;
+				
+				System.out.println(total);
+				
+				contador++;
+
+
+			}
+			
+		}
+		
+
+}
