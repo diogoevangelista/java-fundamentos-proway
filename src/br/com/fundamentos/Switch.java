13ddac64332ab396cdfa0e2bff5d4c6fc164980e
Diogo Evangelista <68126@PROWAY.TREINA>
2020-09-20 18:00:43 -0300

    Trabalhamdo com Switch

---
+++ src/br/com/fundamentos/Switch.java
@@ -0,0 +1,39 @@
+package src.br.com.fundamentos;
+
+import java.util.Scanner;
+
+public class Switch {
+	
+	public static void main (String[] args) {
+	
+		Scanner entrada = new Scanner (System.in);
+		int teste;
+		
+		System.out.println("Digite o mês em número inteiro ");
+		teste = entrada.nextInt();
+		
+			switch (teste) {
+				case 1:
+					System.out.println("Janeiro");
+					break;
+				case 2:
+					System.out.println("Fevereiro");
+					break;
+				case 3:
+					System.out.println("Março");
+					break;
+				case 4:
+					System.out.println("Abril");
+					break;
+					
+				default:
+					System.out.println("Sigite SOMENTE números entre 1 e 4 ");
+					break;
+					
+			
+			}
+			
+		
+	}
+
+}
